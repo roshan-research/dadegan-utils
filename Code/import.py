68031467a0e52b1c4ee33d6abe860827e8f38f6c
Alireza Nourian <alireza.nournia@gmail.com>
2012-03-01 17:29:01 +0330

    import dependency corpus

--- Code/import.py
+++ Code/import.py
@@ -3,11 +3,8 @@
 from pymongo import Connection
 connection = Connection()
 db = connection.dadegan
-valencies = db.valencies
 
-# first remove all valencies
-valencies.remove()
-
+# Valency DB
 def parseValency(valency):
 	valency = valency.decode('utf-8')
 
@@ -79,27 +76,90 @@ def parseValency(valency):
 	
 	return result
 
-file = open('valency.txt')
-l, desc = 0, True
-for line in file:
-	l += 1
+def importValencies():
+	valencies = db.valencies
 
-	# don't add description lines
-	if desc:
-		if line.startswith('------'): desc = False
-		continue
+	# first remove all valencies
+	valencies.remove()
 
-	try:
-		row = [x if x != '-' else None for x in line.strip().split('\t')]
-		verb = {
-			'past tense root': row[0],
-			'present tense root': row[1],
-			'prfix': row[2],
-			'non-verbal element': row[3],
-			'verbal preposition': row[4],
-			'valency': parseValency(row[5]),
+	file = open('valency.txt')
+	l, desc = 0, True
+	for line in file:
+		l += 1
+
+		# don't add description lines
+		if desc:
+			if line.startswith('------'): desc = False
+			continue
+
+		try:
+			row = [x if x != '-' else None for x in line.strip().split('\t')]
+			verb = {
+				'past tense root': row[0],
+				'present tense root': row[1],
+				'prfix': row[2],
+				'non-verbal element': row[3],
+				'verbal preposition': row[4],
+				'valency': parseValency(row[5]),
+			}
+
+			valencies.insert(verb)
+		except:
+			print 'line %d error: %s' % (l, line),
+
+# Dependency DB
+def makeTree(sentence):
+	def getChilds(parent):
+		words = []
+		for word in sentence:
+			if word[6] == parent:
+				words.append(word)
+		return words
+
+	def makeNode(node):
+		word = {
+			'surface': node[1],
+			'lemma': node[2],
+			'POS': node[3],
+			'POSdetail': node[4],
+			'desc': node[5],
 		}
 
-		valencies.insert(verb)
-	except:
-		print 'line %d error: %s' % (l, line),
+		childs = getChilds(node[0])
+		if childs:
+			word['childs'] = {}
+			for child in childs:
+				key = child[7]
+				while key in word['childs']: key += 'x'
+				word['childs'][key] = makeNode(child)
+
+		return word
+
+	root = getChilds('0')[0]
+	return makeNode(root)
+
+def importDependencies():
+	dependencies = db.dependencies
+
+	# first remove all dependencies
+	dependencies.remove()
+
+	file = open('dependency.conll')
+	l, sentence = 0, []
+	for line in file:
+		parts = line.decode('utf-8').split("\t")
+
+		if len(parts) < 2:
+			tree = makeTree(sentence)
+			dependencies.insert(tree)
+			
+			l += 1
+			sentence = []
+		else:
+			sentence.append(parts)
+
+	print '%d sentences imported' % l
+
+if __name__ == "__main__":
+	importValencies()
+	importDependencies()
