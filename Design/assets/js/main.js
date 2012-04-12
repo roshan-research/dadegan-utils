7e45621ca1d1bb588aa9cdf5635fbce7b25a19e3
Yasser Souri <yassersouri@gmail.com>
2012-04-12 11:46:52 +0430

    test

--- Design/assets/js/main.js
+++ Design/assets/js/main.js
@@ -5,11 +5,21 @@ $(document).ready(function() {
 		var pr = $(this).parent().parent();
 		var spans = pr.find('div.example span.' + cl);
 		spans.css('background-color', 'rgba(30,0,100, 0.5)');
+		addcolor(e);
 	}
 	, function(e){ //Exits
 		var cl = $(this).attr('class');
 		var pr = $(this).parent().parent();
 		var spans = pr.find('div.example span.' + cl);
 		spans.css('background-color', '');
+		substractcolor(e);
 	});
 });
+
+var addcolor = function(color){
+	alert('enter');
+}
+
+var substractcolor = function(color) {
+	alert('exit');
+}
